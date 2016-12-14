91a3c01d2ab55b46494402cf0a275fc4c80e76aa
Naver <Naver@wildrunner-pro.local>
2016-12-14 11:32:59 +0900

    add`

---
+++ labs/hunkim/cost_minimize.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+
+# data set
+x_data = [1., 2., 3., 4.]
+y_data = [1., 3., 5., 7.]
+
+# range is -10000 ~ 10000
+W = tf.Variable(tf.random_uniform([1], -10., 10.))
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+# my hypothesis
+hyphthesis = W * X
+
+# Simplified cost function
+cost = tf.reduce_mean(tf.square(hyphthesis - Y))
+
+# minimize
+descent = W - tf.mul(0.1, tf.reduce_mean(tf.mul((tf.mul(W, X) - Y), X)))
+update = W.assign(descent)
+
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+for step in range(20):
+    sess.run(update, feed_dict={X:x_data, Y:y_data})
+    print(step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run(W))
+
+print(sess.run(hyphthesis, feed_dict={X: 5}))
+print(sess.run(hyphthesis, feed_dict={X: 2.5}))
+
+
+
+
+
