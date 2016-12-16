c91f01704013465917174f57e99f986a55c60b3f
Naver <Naver@wildrunner-pro.local>
2016-12-16 14:52:22 +0900

    add`

--- labs/tensorflow_blog/diabetes.py
+++ labs/tensorflow_blog/diabetes.py
@@ -1,2 +1,12 @@
 from sklearn import datasets
+from pylab import *
 
+diabetes = datasets.load_diabetes()
+print(diabetes.data.shape, diabetes.target.shape)
+print(diabetes.target[:10])
+print(diabetes.data[:10])
+
+plt.scatter(diabetes.data[:, 2], diabetes.target)
+plt.xlabel('x')
+plt.ylabel('y')
+show()
